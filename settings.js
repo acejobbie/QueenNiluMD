c3f9dcb23833116de69091dfaebef783b5811baf
Ace <121753552+acejobbie@users.noreply.github.com>
2023-03-05 16:31:26 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,34 +15,34 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['254113200483'] //ur owner number
+global.ownername = "ᵏⁱʳᵏ🇦🇱 ⃝🅐︎𝗰𝗲 ɨ𝗇ƒɾα🅱️𝖑αᥴᥴ🔥📵" //ur owner name
+global.ytname = "YT: Ace" //ur yt chanel name
+global.socialm = "GitHub: acejobbie" //ur github or insta name
+global.location = "Kenya, Nakuru, Naks" //ur location
 
 //bot bomdy 
-global.ownernomer = "916909137213" //ur number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot MD V6' //ur bot name
+global.ownernomer = "254113200483" //ur number
+global.premium = ['254113200483'] //ur premium number
+global.botname = '🅐︎𝗰𝗲✞︎𝐁𝐎𝐓 MD V6' //ur bot name
 global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot\n\n\nA whatsapp bot developed by\n🦄Dream Guy Xeon\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.themeemoji = "🇦🇱" //ur theme emoji
+global.packname = "Sticker By ꀤ_αｍ_🅐︎𝗰𝗲✞︎.ᵏⁱʳᵏ🇦🇱 ⃝🅐︎𝗰𝗲 ɨ𝗇ƒɾα🅱️𝖑αᥴᥴ🔥📵" //ur sticker watermark packname
+global.author = "Ace Jobbie\n\n\nA whatsapp bot developed by\n🇦🇱Dream Guy Ace\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213" //ur sticker watermark author
+global.wm = "Ace Bot Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
-global.prefa = ['','!','.','#','&']
+global.prefa = [',']
 global.sp = ''
 
 //messages
 global.mess = {
     success: 'Here you go!',
-    admin: 'This feature could be used by admins only!',
-    botAdmin: 'Bot Must Be Admin First!',
+    admin: 'This feature could be used by admins only👽!',
+    botAdmin: 'Bot Must Be Admin First😢!',
     premime: 'Premium Special Features If You Want to Register Type Rent',
     owner: 'This feature could be used by owner only',
     group: 'Features Used Only For Groups!',
